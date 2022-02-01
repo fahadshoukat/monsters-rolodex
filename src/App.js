29aa7c0366c929f824532ff2b812daab4c30047d
fahadshoukat <fraza6970@gmail.com>
2022-02-01 23:58:37 +0500

    add and style searchfield component and add searchFiled functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 
 import { CardList } from "./components/card-list/card-list";
-import './App.css';
+import "./App.css";
+import { SearchBox } from "./components/searchBox/searchBox";
 
 class App extends Component {
-
   constructor() {
     super();
-    
-    this.state = {
-      monsters: []
-    }
-  };
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div>
-        <CardList monsters={this.state.monsters} />      
+        <SearchBox 
+          placeholder="search monster"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
